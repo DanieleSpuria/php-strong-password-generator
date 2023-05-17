df2bf1b82e04f5ba87eb60826e829811c4a4e50b
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-05-17 19:01:11 +0200

    add style

--- index.php
+++ index.php
@@ -5,14 +5,25 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
+  <style>
+    body {
+      height: 100vh;
+    }
+
+    .container {
+      height: 100%;
+    }
+  </style>
   <title>PHP Strong Password Generator</title>
 </head>
 <body>
 
-  <form action="./password.php" method="GET">
-    <input type="number" name="length" placeholder="Inserisci lunghezza password">
-    <button type="submit">Invia</button>
-  </form>
+  <div class="container d-flex justify-content-center align-items-center text-center">
+    <form action="./password.php" method="GET">
+      <input type="number" name="length" class="input-group my-5" placeholder="Inserisci lunghezza password">
+      <button class="btn btn-primary " type="submit">Invia</button>
+    </form>
+  </div>
 
 </body>
 </html>
--- password.php
+++ password.php
@@ -15,9 +15,21 @@ if (!empty($length) && isset($length)) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
+  <style>
+    body {
+      height: 100vh;
+    }
+
+    .container {
+      height: 100%;
+    }
+  </style>
   <title>Password</title>
 </head>
 <body>
-  <h1>Password generata: <?php echo generatorPass($length) ?></h1>
+  <div class="container d-flex justify-content-center align-items-center">
+    <h1>Password generata: "<?php echo generatorPass($length) ?>"</h1>
+  </div>
 </body>
 </html>
