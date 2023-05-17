c311737b237535e320620b36d0de4885bf941fe3
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-05-17 18:07:19 +0200

    add functions.php

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+  
+  function generatorPass($length) {
+    if (!empty($length) && isset($length)) {
+      $symbolsPass = 'abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPQRSTUVXYZ123456789!?&%$<>^+-*/()[]{}@#_=';
+      $password = '';
+  
+      for ($i = 0; $i < $length; $i++) {
+        $n = rand(0, strlen($symbolsPass) - 1);
+        $password .= $symbolsPass[$n];
+      }
+      return $password;
+    }
+  }
--- index.php
+++ index.php
@@ -2,17 +2,7 @@
 
   $length = $_GET['length'];
 
-  function generatorPass($length) {
-    $symbolsPass = 'abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPQRSTUVXYZ123456789!?&%$<>^+-*/()[]{}@#_=';
-    $password = '';
-
-    for ($i = 0; $i < $length; $i++) {
-      $n = rand(0, strlen($symbolsPass) - 1);
-      $password .= $symbolsPass[$n];
-    }
-
-    return $password;
-  }
+  include __DIR__.'/functions.php';
 
 
 ?>
