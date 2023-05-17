a42740e285dde7e15d82b16d014678a406aff585
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-05-17 16:28:40 +0200

    add form

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+
+  $length = $_GET['length'];
+
+  var_dump($length)
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,5 +16,11 @@
   <title>PHP Strong Password Generator</title>
 </head>
 <body>
+
+  <form action="./index.php" method="GET">
+    <input type="number" name="length" placeholder="Inserisci lunghezza password">
+    <button type="submit">Invia</button>
+  </form>
+
 </body>
 </html>
